fa1047a971ec0f1e9a0b33e7dd7bb32b306d9d13
Olegstasiw <Olegstasiw@icloud.com>
2022-01-08 15:59:22 +0200

    Added Models

---
+++ Your Grade Book/Models/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  Your Grade Book
+//
+//  Created by mac on 08.01.2022.
+//
+
+struct Task {
+    let name: String
+    let maxPoint: Int
+    let systemType: System
+    let points: [CurrentTask]
+}
